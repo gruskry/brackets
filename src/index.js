d3046092ad10bdf6ed6b5b0b3878660a2e143dc9
gruskry <60567825+gruskry@users.noreply.github.com>
2020-03-22 12:31:38 +0300

    feat: upload solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    str = str.split('');
+    for(let i = 0; i < str.length; i++){
+        for(let j = 0; j < bracketsConfig.length; j++){
+            if(bracketsConfig[j][0] === str[i] && bracketsConfig[j][1] === str[i+1]){
+                str.splice(i, 2);
+                i = -1;
+            }
+        }
+    }
+    
+    return str.length == 0;
 }
